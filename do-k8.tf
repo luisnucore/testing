0dbc501618a81f2cf4f915ba6d2f551b071b8203
Luis Rios <luis.rios@nucore.mx>
2020-04-04 10:53:41 +0000

    k8 cluster deploy no issues

--- do-k8.tf
+++ do-k8.tf
@@ -1,3 +1,8 @@
+variable "trabajadores" {
+  default = 1
+}
+
+
 resource "digitalocean_droplet" "do-master" {
     image = "ubuntu-18-04-x64"
     name = "do-master"
@@ -23,6 +28,7 @@ provisioner "remote-exec" {
       # install docker
       "sudo apt-get update",
       "sudo apt install -y docker.io",
+      "systemctl enable docker.service",
       # install kubeadm
       "apt install -y curl",
       "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add",
@@ -30,14 +36,20 @@ provisioner "remote-exec" {
       "apt install -y kubeadm",
       "kubeadm version",
       "swapoff -a",
+      "kubeadm init --pod-network-cidr=10.244.0.0/16 --token rdzukz.38nvax5kpjg3hfof",
+      "mkdir -p $HOME/.kube",
+      "cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+      "chown $(id -u):$(id -g) $HOME/.kube/config",
+      "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
     ]
   }
 }
 
 
-resource "digitalocean_droplet" "do-slave" {
+resource "digitalocean_droplet" "do-worker" {
+    count    = "${var.trabajadores}"
     image = "ubuntu-18-04-x64"
-    name = "do-slave"
+    name = "do-slave${count.index}"
     region = "nyc3"
     size = "2gb"
     private_networking = true
@@ -49,7 +61,7 @@ resource "digitalocean_droplet" "do-slave" {
 connection {
       user = "root"
       agent = true
-      host     = "${digitalocean_droplet.do-slave.ipv4_address}"
+      host     = self.ipv4_address
       type = "ssh"
       private_key = "${file(var.pvt_key)}"
       timeout = "2m"
@@ -61,6 +73,7 @@ provisioner "remote-exec" {
       # install docker
       "sudo apt-get update",
       "sudo apt install -y docker.io",
+      "systemctl enable docker.service",
       # install kubeadm
       "apt install -y curl",
       "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add",
@@ -68,7 +81,8 @@ provisioner "remote-exec" {
       "apt install -y kubeadm",
       "kubeadm version",
       "swapoff -a",
+      "sudo kubeadm join ${digitalocean_droplet.do-master.ipv4_address_private}:6443 --token rdzukz.38nvax5kpjg3hfof --discovery-token-unsafe-skip-ca-verification",
     ]
   }
 }
-
+ 
